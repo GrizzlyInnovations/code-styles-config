01ea317bef1601431ced71ed009c746d37085698
Shannon Griswold <shannongriswold@grizzlyinnovations.com>
2023-04-26 11:27:31 -0600

    more rules

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "./lib/index.js",
+  ],
+  ignorePatterns: [
+    'node_modules',
+  ],
+}; 
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import {
+  ESLint,
+  Linter,
+} from 'eslint';
+
+export const func = () => {
+  console.log('here is a thing');
+
+  const linter = Linter;
+  const lint = ESLint;
+};
