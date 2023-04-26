2630e787b20ecc21700cc6ab25def605c9b5e48d
Shannon Griswold <shannongriswold@grizzlyinnovations.com>
2023-04-26 10:24:23 -0600

    drop .idea, export and rule tweaks

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+export default {
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "unused-imports",
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: [
+          "node_modules",
+          "src/",
+        ],
+      },
+    },
+  },
+  rules: {
+    "indent": 2,
+    "compact": true,
+    "unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          "parent",
+          ["sibling", "index"],
+          "object",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    complexity: "warn",
+    "no-console": [
+      "error",
+    ],
+  },
+};
